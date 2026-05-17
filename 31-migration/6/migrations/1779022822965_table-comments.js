cd56549b748e1a95215a55721bb296359593b196
vikas-cse123 <sagarraothakur2@gmail.com>
2026-05-17 19:10:26 +0530

    Section 31

---
+++ 31-migration/6/migrations/1779022822965_table-comments.js
@@ -0,0 +1,31 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+         CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    contents VARCHAR(240) NOT NULL
+  );
+        
+        
+        `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`DROP TABLE comments`);
+};
